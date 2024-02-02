7bcadce31581807e194df76262e24c430bfad261
Spolpinka <a421243266@gmail.com>
2024-02-02 07:43:01 +0300

    доработано

--- src/main/java/org/mukhortov/Main.java
+++ src/main/java/org/mukhortov/Main.java
@@ -1,8 +1,10 @@
 package org.mukhortov;
 
 import org.mukhortov.requests.ExcelWork;
-import org.mukhortov.requests.PiwiRequest;
+import org.mukhortov.requests.TwoGisRequest;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -11,19 +13,23 @@ public class Main {
     private int timeForControlInSec;
 
 
+
+
     public static void main(String[] args) {
         Main main = new Main();
         String file = main.getFile();
 
         ExcelWork excelWork = new ExcelWork();
-        excelWork.openFileAndRead(file);
+        Map<Integer, List<String>> data = excelWork.openFileAndRead(file);
 
+        TwoGisRequest tgRequest = new TwoGisRequest();
+        data = tgRequest.transformData(data);
 
-
+        excelWork.dataWriting(data);
     }
 
     private String getFile() {
-        System.out.println("Введите полное имя файла для выгрузки анализа данных:");
+        System.out.println("Введите полное имя файла для выгрузки и анализа данных:");
         try {
             file = scan.nextLine();
         } catch (Exception e) {
--- src/main/java/org/mukhortov/requests/ExcelWork.java
+++ src/main/java/org/mukhortov/requests/ExcelWork.java
@@ -1,13 +1,11 @@
 package org.mukhortov.requests;
 
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.mukhortov.Main;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +13,7 @@ import java.util.Map;
 
 public class ExcelWork {
 
-    public void openFileAndRead(String fileLocation) {
+    public Map<Integer, List<String>> openFileAndRead(String fileLocation) {
         Workbook workbook = null;
         try {
             FileInputStream file = new FileInputStream(new File(fileLocation));
@@ -52,7 +50,7 @@ public class ExcelWork {
             i++;
         }
 //Поверяем данные
-        System.out.println("Проверяем считанное");
+        /*System.out.println("Проверяем считанное");
         for (List list :
                 data.values()) {
             for (Object s :
@@ -60,6 +58,88 @@ public class ExcelWork {
 
                 System.out.println(s);
             }
+        }*/
+        return data;
+    }
+
+    public void dataWriting(Map<Integer, List<String>> data) {
+
+        //создали книгу, видим фигу
+        Workbook workbook = new XSSFWorkbook();
+        System.out.println("нулевая строка: ");
+        for (String s : data.get(0)) {
+            System.out.print(s + "|");
+        }
+        int rowSize = data.get(0).size();
+//лист
+        Sheet sheet = workbook.createSheet("OSP Coordinates");
+        sheet.setColumnWidth(0, rowSize);
+        sheet.setColumnWidth(1, data.size());
+
+        /*Row header = sheet.createRow(0);
+//меняем стиль
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Times New Roman");
+        font.setFontHeightInPoints((short) 14);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue("OSP ID");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Address");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(2);
+        headerCell.setCellValue("Lat");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(3);
+        headerCell.setCellValue("Lon");
+        headerCell.setCellStyle(headerStyle);*/
+
+        //пишем содержимое
+        CellStyle style = workbook.createCellStyle();
+        style.setWrapText(true);
+
+        for (int i = 0; i < data.size(); i++) {
+            List<String> tmpList = data.get(i);
+            Row row = sheet.createRow(i);
+
+            for (int x = 0; x < tmpList.size(); x++) {
+                Cell cell = row.createCell(x);
+                cell.setCellValue(tmpList.get(x));
+                cell.setCellStyle(style);
+            }
+        }
+        /*for (Map.Entry<Integer, List<String>> dataRow:
+             data.entrySet()) {
+            Row row = sheet.createRow(dataRow.getKey()+1);
+            for (int x = 0; x < dataRow.getValue().size(); x++) {
+                Cell cell = row.createCell(x);
+                cell.setCellValue(dataRow.getValue().get(x));
+                cell.setCellStyle(style);
+            }
+        }*/
+
+
+//пишем в файл, надо в наш
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = "C:\\Users\\user\\Documents\\МОЛЛ\\" + "temp.xlsx";
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileLocation);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (IOException e) {
+            System.out.println("Запись не работает " + e.getMessage());
         }
     }
 }
